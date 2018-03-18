ef8ff7b5a6c3eec581a3c3225f1e316472aa680f
HeoliaJdR <staff@heoliajdr.net>
2018-03-18 18:47:01 +0100

    verif

--- function.c
+++ function.c
@@ -5,6 +5,10 @@
 #include "function.h"
 
 
+void destroyWindow(GtkWidget *widget, gpointer data) {
+    gtk_main_quit();
+}
+
 GError *loadGladeFile(GtkBuilder **builder, char *fileName) {
     GError *error = NULL;
 
@@ -13,43 +17,6 @@ GError *loadGladeFile(GtkBuilder **builder, char *fileName) {
     gtk_builder_add_from_file(*builder, fileName, &error);
 
     return error;
-}
-
-int defineEntry (int positionVerif, char *actualEntry){
-    switch (positionVerif) {
-      case 0:
-        strcpy(actualEntry, nameEntry);
-        break;
-      case 1:
-        strcpy(actualEntry, surnameEntry);
-        break
-      case 2:
-        strcpy(actualEntry, dateEntry);
-        break;
-      case 3:
-        strcpy(actualEntry, secuEntry);
-        break
-      case 4:
-        strcpy(actualEntry, emailEntry);
-        break;
-      case 5:
-        strcpy(actualEntry, postEntry);
-        break
-      case 6:
-        strcpy(actualEntry, restaurantEntry);
-        break;
-    }
-    return 0;
-}
-
-int selectEntry (GtkWidget *entry, char *actualEntry, int positionVerif){
-    defineEntry(positionVerif, actualEntry);
-    entry = GTK_WIDGET(gtk_builder_get_object(builder, actualEntry));
-    if(entry != NULL)
-      return 0;
-    else
-      return -1;
-
 }
 
 int textVerification(char *actualEntry, int *entryVerification, int positionVerif){
@@ -90,7 +57,8 @@ int emailVerification(char *actualEntry, int *entryVerification, int positionVer
 }
 
 int dateVerification(char *actualEntry, int *entryVerification, int positionVerif){
-  return entryVerification[positionVerif] = 1;
+  if(actualEntry != NULL)
+    return entryVerification[positionVerif] = 1;
 }
 
 int secuVerification(char *actualEntry, int *entryVerification, int positionVerif){
@@ -123,39 +91,248 @@ int postOrRestaurantVerification(char *actualEntry, int *entryVerification, int 
   }
 }
 
-int entryVerification (int *entryVerification, GtkWidget *entry, int positionVerif, char *actualEntry, char **savedEntry){
-    selectEntry(entry, actualEntry, positionVerif);
+int entryVerification (int positionVerif, char *actualEntry, int *entryVerification){
+    selectEntry(actualEntry, positionVerif);
     switch (positionVerif) {
       case 0:
         textVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break;
       case 1:
         textVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break
       case 2:
         dateVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break;
       case 3:
         secuVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break
       case 4:
         emailVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break;
       case 5:
         postOrRestaurantVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break
       case 6:
         postOrRestaurantVerification(actualEntry, entryVerification, positionVerif);
+        &positionVerif++;
         break;
     }
 }
 
-int saveEntry (int *entryVerification, GtkWidget *entry, int positionVerif, char **savedEntry){
-  if(actualEntry != NULL){
-    savedEntry[positionVerif] = "y";
-    savedEntry[positionVerif][positionVerif] = actualEntry;
-    return 0;
-  }
-  else
-    return -1;
+void checkWorkerInfo(GtkWidget *widget, gpointer data, int *entryVerification){
+
+    GtkWidget *tempWidget;
+    Validate workerInfo;
+    GError *error;
+    GtkWidget *window;
+    GtkBuilder *builder;
+    int positionVerif = 0;
+
+    /**
+     * Je récupère mon champs
+     * Je récupère le contenu de mon champ et je le stocke dans ma structure
+     */
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"SurnameEntry");
+    workerInfo.surname = (char *) gtk_entry_get_text(GTK_ENTRY(tempWidget));
+    entryVerification(positionVerif, workerInfo.surname, entryVerification);
+
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"NameEntry");
+    workerInfo.name = (char *) gtk_entry_get_text(GTK_ENTRY(tempWidget));
+    entryVerification(positionVerif, workerInfo.name, entryVerification);
+
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"BirthdayEntry");
+    workerInfo.birthday = (char *) gtk_entry_get_text(GTK_ENTRY(tempWidget));
+    entryVerification(positionVerif, workerInfo.birthday, entryVerification);
+
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"NumSecuEntry");
+    workerInfo.numsecu = (char *) gtk_entry_get_text(GTK_ENTRY(tempWidget));
+    entryVerification(positionVerif, workerInfo.numsecu, entryVerification);
+
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"EmailEntry");
+    workerInfo.email = (char *) gtk_entry_get_text(GTK_ENTRY(tempWidget));
+    entryVerification(positionVerif, workerInfo.email, entryVerification);
+
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"RestaurantComboBox");
+    workerInfo.restaurant = (char *) gtk_combo_box_get_active_id(GTK_COMBO_BOX(tempWidget));
+    entryVerification(positionVerif, workerInfo.restaurant, entryVerification);
+
+    tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(data),"PosteComboBox");
+    workerInfo.poste = (char *) gtk_combo_box_get_active_id(GTK_COMBO_BOX(tempWidget));
+    entryVerification(positionVerif, workerInfo.poste, entryVerification);
+
+
+    if((error = loadGladeFile(&builder,"infoQrcode.glade")) == NULL && positionVerif == 7){
+        window = (GtkWidget *) gtk_builder_get_object(builder,"dialog1");
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(builder,"CloseButton");
+
+        g_signal_connect(G_OBJECT(tempWidget), "clicked", G_CALLBACK(closeDialogBox),(gpointer) window);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"SurnameResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.surname);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"NameResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.name);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"DateResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.birthday);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"SecuResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.numsecu);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"EmailResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.email);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"PosteResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.poste);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"RestaurantResultLabel");
+        gtk_label_set_text(GTK_LABEL(tempWidget), workerInfo.restaurant);
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(GTK_BUILDER(builder),"QrcodeImage");
+        qrCodeToGtkImage(tempWidget, workerInfo.numsecu);
+
+        gtk_widget_show_all(window);
+
+        gtk_main();
+    }else
+        puts(error->message);
+
+}
+
+void openCalendar(GtkEntry *widget, GtkEntryIconPosition iconPos, GdkEvent *event, gpointer *data) {
+    GtkBuilder *builder = NULL;
+    GError *error = NULL;
+    GtkWidget *window = NULL;
+    GtkWidget *calendar = NULL;
+    WindowCalendarParam *calendarParam = (WindowCalendarParam *) data;
+
+    error = loadGladeFile(&builder, "calendar.glade");
+
+    if (error == NULL) {
+        window = (GtkWidget *) gtk_builder_get_object(builder, "calendarWindow");
+
+        if (window != NULL) {
+
+            calendarParam->calendarWindow = window;
+
+            calendar = (GtkWidget *) gtk_builder_get_object(builder, "mainCalendar");
+
+            if (calendarParam->day != -1)
+                gtk_calendar_select_day(GTK_CALENDAR(calendar), (guint) calendarParam->day);
+
+            if (calendarParam->month != -1 && calendarParam->year != -1)
+                gtk_calendar_select_month(GTK_CALENDAR(calendar), (guint) calendarParam->month,
+                                          (guint) calendarParam->year);
+
+            if (calendar != NULL)
+                g_signal_connect(G_OBJECT(calendar), "day-selected-double-click", G_CALLBACK(daySelect),
+                                 calendarParam);
+
+            gtk_widget_show_all(window);
+        } else {
+            printf("No window to display");
+        }
+    } else {
+        printf("%s \n", error->message);
+        g_error_free(error);
+    }
+}
+
+void daySelect(GtkCalendar *calendar, gpointer *data) {
+    WindowCalendarParam *calendarParam = (WindowCalendarParam *) data;
+    guint year = 0;
+    guint day = 0;
+    guint month = 0;
+    gchar tempChar[250];
+
+    gtk_calendar_get_date(calendar, &year, &month, &day);
+
+    gtk_widget_destroy(calendarParam->calendarWindow);
+
+    sprintf(tempChar, "%d-%02d-%02d", year, month + 1, day);
+
+    if (calendarParam->destinationWidget != NULL)
+        gtk_entry_set_text(GTK_ENTRY(calendarParam->destinationWidget), tempChar);
+
+    calendarParam->day = day;
+    calendarParam->month = month;
+    calendarParam->year = year;
+}
+
+void closeDialogBox(GtkWidget *widget, gpointer *data) {
+    GtkWidget *window = (GtkWidget *) data;
+
+    gtk_widget_destroy(window);
+
+
+}
+
+void qrCodeToGtkImage(GtkWidget **image, char *code){
+
+    char *array = NULL;
+    GtkWidget *window;
+    GtkWidget *picture;
+    GtkWidget *widget;
+    GtkBuilder *builder;
+    GdkPixbuf * pixbuf;
+    int option = 0;
+    int border = 12;
+    int allBorder = border *2;
+    int grow = 6;
+
+
+    uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
+    uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+    bool ok = qrcodegen_encodeText(code,
+                                   tempBuffer, qr0, qrcodegen_Ecc_MEDIUM,
+                                   qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX,
+                                   qrcodegen_Mask_AUTO, true);
+    if (!ok)
+        return;
+
+    int size = qrcodegen_getSize(qr0);
+
+    /**
+     * Allocate the array
+     */
+    array = malloc(sizeof(char)* ( ((size * grow) + allBorder) * ((size * grow) + allBorder) * 3));
+
+    memset(array, 255, (size_t) ((size * grow) + allBorder) * ((size * grow) + allBorder) * 3);
+
+    for (int y = 0; y < size; y++) {
+
+        for (int j = y * grow; j < (y * grow) + grow; ++j) {
+
+            for (int x = 0; x < size; x++) {
+
+                for (int i = 0; i < grow; ++i){
+                    option = (j * ((size * grow) + allBorder) * 3) + (x * grow * 3) + (border * 3) + (i * 3) + (border * ((size * grow) + allBorder) * 3);
+                    if(qrcodegen_getModule(qr0, x, y) == 1){
+                        array[option] = (char) 0;
+                        array[option + 1] = (char) 0;
+                        array[option + 2] = (char) 0;
+                    }
+                }
+            }
+        }
+    }
+
+    pixbuf = gdk_pixbuf_new_from_data((const guchar*) array,
+                                      GDK_COLORSPACE_RGB,
+                                      FALSE,
+                                      8,
+                                      (size * grow) + allBorder,
+                                      (size * grow) + allBorder,
+                                      ((size * grow) + allBorder) * 3,
+                                      NULL,
+                                      NULL);
+
+    gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
 }
--- main.c
+++ main.c
@@ -3,76 +3,53 @@
 #include "qrcodegen.h"
 #include <gtk/gtk.h>
 #include "function.h"
-#include "constant.h"
 
 int main(int argc,char** argv) {
     char *array = NULL;
-    char *actualEntry = NULL;
-    char **savedEntry = NULL;
     int *entryVerification = NULL;
 
 
     GtkWidget *window;
-    GtkWidget *picture;
+    GtkWidget *tempWidget;
     GtkBuilder *builder;
-    GtkWidget *entry;
     GdkPixbuf * pixbuf;
+    GError *error;
+    WindowCalendarParam *tempCalendarParam = NULL;
 
-// Allocation mémoire du tableau de verification des entry
+    // Allocation mémoire du tableau de verification des entry
     entryVerification = malloc(sizeof(int)*7);
-    actualEntry = malloc(sizeof(char)*20);
-    savedEntry = malloc(sizeof(char)*7);
-    for(int i = 0; i < 7; i++){
-      savedEntry[i] = malloc(sizeof(char)*20);
-    }
-
+    
     gtk_init(&argc,&argv);
-    loadGladeFile(&builder,"QRCODE.glade");
-    window = (GtkWidget *) gtk_builder_get_object(builder,"windowQrcode");
+    if((error = loadGladeFile(&builder,"infoQR.glade")) == NULL){
+        window = (GtkWidget *) gtk_builder_get_object(builder,"window1");
 
-// Text data
-    uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
-    uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
-    bool ok = qrcodegen_encodeText("Hello, world!",
-                                   tempBuffer, qr0, qrcodegen_Ecc_MEDIUM,
-                                   qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX,
-                                   qrcodegen_Mask_AUTO, true);
-    if (!ok)
-        return 0;
+        g_signal_connect(window, "destroy", G_CALLBACK(destroyWindow), NULL);
 
-    int size = qrcodegen_getSize(qr0);
-    array = malloc(sizeof(char)*((size + 6)*(size+6)*3));
-    for (int i = 0; i < 3 * ((size+6)*3); ++i) {
-        array[i] = (char) 255;
-    }
-    for (int y = 0; y < size; y++) {
-        for (int x = 0; x < size + 6; x++) {
-            printf("%d \n", (y * size * 3) + (x * 3) + (3*size*3));
-            if(x < 3 || x > size + 3){
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3)] = (char) 255;
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3) + 1] = (char) 255;
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3) + 2] = (char) 255;
-            }
-            else if(qrcodegen_getModule(qr0, x - 3, y) == 0 ){
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3)] = (char) 255;
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3) + 1] = (char) 255;
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3) + 2] = (char) 255;
-            }else{
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3)] = (char) 0;
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3) + 1] = (char) 0;
-                array[(y * (size + 6) * 3) + (x * 3) + (3 * (size + 6) * 3) + 2 ] = (char) 0;
-            }
+        tempWidget = (GtkWidget *) gtk_builder_get_object(builder, "BirthdayEntry");
+
+        if (tempWidget != NULL) {
+
+            tempCalendarParam = (WindowCalendarParam *) calloc(1, sizeof(WindowCalendarParam));
+            tempCalendarParam->destinationWidget = tempWidget;
+            tempCalendarParam->calendarWindow = NULL;
+            tempCalendarParam->year = -1;
+            tempCalendarParam->month = -1;
+            tempCalendarParam->day = -1;
+
+            g_signal_connect(G_OBJECT(tempWidget), "icon-press", G_CALLBACK(openCalendar), (gpointer) tempCalendarParam);
+
         }
-    }
-    printf("%d \n", (size*(size*3)) + (3 * (size * 3)));
-    for (int i = (size* (size + 6) * 3) + (3 * ((size + 6) * 3)); i < (size* (size + 6) * 3) + (6 * ((size + 6) * 3)); ++i) {
-        array[i] = (char) 255;
-    }
-    pixbuf = gdk_pixbuf_new_from_data((const guchar*) array,GDK_COLORSPACE_RGB,FALSE,8,size + 6,size+6,(size + 6) * 3,NULL,NULL);
-    picture =(GtkWidget *) gtk_builder_get_object(builder,"etapeImage");
-    pixbuf = gdk_pixbuf_scale_simple(pixbuf,200,200,GDK_INTERP_HYPER);
-    gtk_image_set_from_pixbuf(GTK_IMAGE(picture),pixbuf);
-    gtk_widget_show_all(window);
-    gtk_main();
+
+        tempWidget = (GtkWidget *) gtk_builder_get_object(builder,"validerButton");
+
+        g_signal_connect(G_OBJECT(tempWidget), "clicked", G_CALLBACK(checkWorkerInfo),(gpointer) builder);
+
+        gtk_widget_show_all(window);
+
+        gtk_main();
+    }else
+        puts(error->message);
+
+
     return 0;
 }
